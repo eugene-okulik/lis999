98f72ea72afb69f61945aedb3fe68b27acb20651
Sergey Li <sergo.li.seattle@gmail.com>
2025-04-14 21:07:45 -0700

    Function has been improved

--- homework/sergey_li/Homework_5/HW_5_Task_2.py
+++ homework/sergey_li/Homework_5/HW_5_Task_2.py
@@ -4,13 +4,10 @@ result_3 = "результат работы программы: 9"
 
 
 def change_result(result):
-    return int(result[20:]) + 10
-
-
-def change_result_2(result):
-    return int(result[28:]) + 10
+    words = result.split(" ")
+    return int(words[-1]) + 10
 
 
 print(change_result(result_1))
 print(change_result(result_2))
-print(change_result_2(result_3))
+print(change_result(result_3))
