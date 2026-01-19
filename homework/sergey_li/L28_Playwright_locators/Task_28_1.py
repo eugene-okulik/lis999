4e51f0dd0b73844ba4a7bc32c3feb5cc32fe511d
Sergey Li <sergo.li.seattle@gmail.com>
2026-01-19 14:53:01 -0800

    added test practice page

--- homework/sergey_li/L28_Playwright_locators/Task_28_1.py
+++ homework/sergey_li/L28_Playwright_locators/Task_28_1.py
@@ -4,14 +4,26 @@ from time import sleep
 
 
 def test_authentication(page: Page):
-    sleep(3)
     page.goto("https://the-internet.herokuapp.com/")
     page.get_by_role("link", name="Form Authentication").click()
-    sleep(3)
     username_field = page.get_by_role("textbox", name="username")
     username_field.fill("username")
     password_field = page.get_by_role("textbox", name="password")
     password_field.fill("password")
-    sleep(3)
     page.get_by_role("button", name="Login").click()
+
+
+def test_practice_form(page: Page):
+    page.goto("https://demoqa.com/automation-practice-form")
+    first_name = page.get_by_role("textbox", name="First Name")
+    first_name.fill("Sergo")
+    last_name = page.get_by_placeholder("Last Name")
+    last_name.fill("Lee")
+    email = page.locator("#userEmail")
+    email.press_sequentially("sergo@gmail.com", delay=500)
+    sleep(3)
+    page.locator("#gender-radio-1").click()
+    sleep(3)
+    mobile = page.get_by_placeholder("Mobile Number")
+    mobile.fill("4252555225")
     sleep(3)
