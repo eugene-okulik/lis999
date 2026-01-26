d31f6d4e07fd61d65f237df51528af560f9267f8
Sergey Li <sergo.li.seattle@gmail.com>
2026-01-25 21:15:12 -0800

    created 3 test

--- homework/sergey_li/L29_Playwright_Additions/L29_Task1.py
+++ homework/sergey_li/L29_Playwright_Additions/L29_Task1.py
@@ -9,3 +9,28 @@ def test_acceptance_alert(page: Page):
     page.get_by_role("link", name="Click").click()
     result_text = page.locator("#result-text")
     expect(result_text).to_have_text("Ok")
+
+
+def test_new_tab(page: Page, context: BrowserContext):
+    page.goto("https://www.qa-practice.com/elements/new_tab/button")
+    sleep(3)
+    button = page.locator("#new-page-button")
+    with context.expect_page() as new_page_event:
+        button.click()
+    sleep(3)
+    new_page = new_page_event.value
+    result = new_page.locator("#result-text")
+    expect(result).to_have_text("I am a new page in a new tab")
+    sleep(3)
+    new_page.close()
+    page.locator("#new-page-button").is_enabled()
+    sleep(3)
+
+
+def test_color_button(page: Page):
+    page.goto("https://demoqa.com/dynamic-properties")
+    sleep(3)
+    color_button = page.locator("#colorChange")
+    expect(color_button).to_have_class(re.compile("text-danger"))
+    sleep(3)
+    color_button.click()
