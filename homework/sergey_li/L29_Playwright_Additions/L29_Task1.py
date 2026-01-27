e5e1306bd2de5de3177c8435d7a75efd73545680
Sergey Li <sergo.li.seattle@gmail.com>
2026-01-26 21:14:13 -0800

    minor updates in tests

--- homework/sergey_li/L29_Playwright_Additions/L29_Task1.py
+++ homework/sergey_li/L29_Playwright_Additions/L29_Task1.py
@@ -1,6 +1,5 @@
-from playwright.sync_api import Page, expect, BrowserContext, Dialog
+from playwright.sync_api import Page, expect, BrowserContext
 import re
-from time import sleep
 
 
 def test_acceptance_alert(page: Page):
@@ -13,24 +12,18 @@ def test_acceptance_alert(page: Page):
 
 def test_new_tab(page: Page, context: BrowserContext):
     page.goto("https://www.qa-practice.com/elements/new_tab/button")
-    sleep(3)
     button = page.locator("#new-page-button")
     with context.expect_page() as new_page_event:
         button.click()
-    sleep(3)
     new_page = new_page_event.value
     result = new_page.locator("#result-text")
     expect(result).to_have_text("I am a new page in a new tab")
-    sleep(3)
     new_page.close()
     page.locator("#new-page-button").is_enabled()
-    sleep(3)
 
 
 def test_color_button(page: Page):
     page.goto("https://demoqa.com/dynamic-properties")
-    sleep(3)
     color_button = page.locator("#colorChange")
     expect(color_button).to_have_class(re.compile("text-danger"))
-    sleep(3)
     color_button.click()
